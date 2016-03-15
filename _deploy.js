f4b36b59af406b1d92810d94f090d599afb0fa2a
paulshorey <ps@artspaces.net>
2016-03-14 23:14:33 -0400

    test push

--- _deploy.js
+++ _deploy.js
@@ -29,6 +29,5 @@ app.all('/_deploy', function(req, res) {
 });
 
 http.createServer(app).listen(app.get('port'), function() {
-	
 	console.log('Express server listening on port ' + app.get('port'));
 });
