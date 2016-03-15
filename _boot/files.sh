ca7ec84586b4f49b4974b76ebc66034524c664df
paulshorey <ps@artspaces.net>
2016-03-14 23:39:49 -0400

    deploy sh

--- _boot/files.sh
+++ _boot/files.sh
@@ -3,4 +3,4 @@ iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 40
 cd /www/bot-nyc
 rm -rf public/console/logs
 echo starting http-server
-http-server public -p 4080 -s -c 30 #-d false
+http-server public -p 2080 -s -c 30 #-d false
