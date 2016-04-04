de3a906a0fa6800e5da1c5047679c713c98a63ce
paul shorey <ps@artspaces.net>
2016-04-03 22:43:32 -0400

    tweak sh

--- _boot/deploy.sh
+++ _boot/deploy.sh
@@ -1,4 +1,6 @@
 sleep 20
 
 cd /www/go-nyc
+
+echo node deploy.js starting...
 node _hook/deploy.js
--- _boot/files.sh
+++ _boot/files.sh
@@ -3,6 +3,5 @@ sleep 10
 iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 4080
 cd /www/go-nyc
 
-#rm -rf public/console/logs
-echo http-server starting - port :4080
+echo http-server starting... :4080
 http-server public -p 4080 -s -c 30 #-d false
