45bac32c0abd96c4cf755e5865e54ada80a11269
paul shorey <ps@artspaces.net>
2016-04-03 22:57:12 -0400

    files

--- _boot/files.sh
+++ _boot/files.sh
@@ -2,5 +2,5 @@ sleep 10
 
 cd /www/go-nyc
 
-echo http-server starting... :4080
-http-server public -p 4080 -s -c 30 #-d false
+echo http-server starting... :9080
+http-server public -p 9080 -s -c 30 #-d false
